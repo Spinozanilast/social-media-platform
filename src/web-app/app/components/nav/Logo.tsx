77e9140b2e0ffac608c3f776083a46bf215a8611
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-07-12 20:18:07 +0300

    add registration and signin pages with needed components

--- src/web-app/app/components/nav/Logo.tsx
+++ src/web-app/app/components/nav/Logo.tsx
@@ -1,17 +1,39 @@
-import React from "react";
-import "@fontsource/share-tech-mono";
+"use client";
 import OnDisplayAnimatedSpan from "../utils/OnDisplayAnimatedSpan";
+import "@fontsource/share-tech-mono";
+import { useState } from "react";
 
-export default function Logo() {
+type LogoProps = {
+    animationType: "backward" | "forward";
+};
+
+export default function Logo({ animationType }: LogoProps) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handlePointerEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handlePointerLeave = () => {
+        setIsHovered(false);
+    };
+
+    const animatedSpanStyle =
+        animationType === "forward" ? "group-hover:inline hidden" : "inline";
+
     return (
         <p
             className="group logo text-accent-orange text-3xl"
             style={{ fontFamily: "Share Tech Mono" }}
+            onPointerEnter={handlePointerEnter}
+            onPointerLeave={handlePointerLeave}
         >
             P
             <OnDisplayAnimatedSpan
-                classname="hidden group-hover:inline"
+                animationType={animationType}
+                classname={animatedSpanStyle}
                 duration={2}
+                isHovered={isHovered}
             >
                 latfor
             </OnDisplayAnimatedSpan>
