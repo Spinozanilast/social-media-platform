cc367cf4e0ae8cff06c93368512130d3c2d2e1a1
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-07-17 20:07:40 +0300

    add user identity service to configuration

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", policy =>
+    {
+        policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins(builder.Configuration["ClientApp"]!);
+    });
+});
 var app = builder.Build();
+app.UseCors();
 app.MapReverseProxy();
 app.Run();
