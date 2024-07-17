62474a2df7e06bca3d09c05f2b0583ee5b6a7e6e
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-07-17 20:57:47 +0300

    add custom cors policy

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("allowAny", builder =>
+    {
+        builder.AllowAnyOrigin();
+    });
+});
 var app = builder.Build();
+app.UseCors();
 app.MapReverseProxy();
 app.Run();
