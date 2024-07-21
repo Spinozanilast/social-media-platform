85ffbc847b7df2d1e7ff65f7339cfe0af64fdcd5
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-07-21 11:52:30 +0300

    edit authtextfield and add autofill back color fix to globals.css (sh*t story)

--- src/web-app/app/themes/mui-components/AuthTextField.tsx
+++ src/web-app/app/themes/mui-components/AuthTextField.tsx
@@ -1,22 +1,24 @@
 import { alpha, styled } from "@mui/material";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
-export const AuthTextField = styled((props: TextFieldProps) => (
-    <TextField {...props} />
-))(({ theme }) => ({
+import { forwardRef } from "react";
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
     "& .MuiFilledInput-root": {
         overflow: "hidden",
         borderRadius: 4,
-        border: "1px solid",
-        borderColor:
+        backgroundColor:
             theme.palette.mode === "light"
-                ? //TODO: Color themes applying
-                  theme.palette.text.primary
-                : theme.palette.text.secondary,
+                ? theme.palette.secondary.main
+                : theme.palette.secondary.main,
+        border: "1px solid",
         transition: theme.transitions.create([
             "border-color",
             "background-color",
             "box-shadow",
         ]),
+        "&:hover": {
+            backgroundColor: "transparent",
+        },
         "&.Mui-focused": {
             backgroundColor: "transparent",
             boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 2px`,
@@ -24,3 +26,15 @@ export const AuthTextField = styled((props: TextFieldProps) => (
         },
     },
 }));
+
+// eslint-disable-next-line react/display-name
+export const AuthTextField = forwardRef<HTMLInputElement, TextFieldProps>(
+    (props, ref) => (
+        <StyledTextField
+            InputProps={{ disableUnderline: true }}
+            variant="filled"
+            ref={ref}
+            {...props}
+        />
+    )
+);
