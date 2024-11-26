356a1e64147ec066685f379a22a7bfbbc85fc89f
Vadim Budchanin <107877150+Spinozanilast@users.noreply.github.com>
2024-11-26 19:41:22 +0300

    feat: add docker app settings for docker host gateway interacting with other services

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,7 +1,22 @@
+using Shared.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddConfiguredSerilog(builder.Configuration);
+
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader()
+            .AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]!);
+    });
+});
+
 var app = builder.Build();
-app.UseCors(policyBuilder => policyBuilder.AllowAnyOrigin());
+app.UseCors();
 app.MapReverseProxy();
 app.Run();
